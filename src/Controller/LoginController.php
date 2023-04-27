d4c0214bb83f624cc647316b5c693cf3c66f8b92
Matthieu <matt@boodcode.com>
2023-04-27 18:54:32 +0200

    redirection si déjà logged

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -14,10 +14,20 @@ class LoginController extends AbstractController
     #[Route('/login', name: 'app_login')]
      public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        /**
+         * If the user has already logged in (marked as is authenticated fully by symfony's security)
+         * then redirect this user back (in my case, to the dashboard, which is the main entry for
+         * my logged in users)
+         */
+        //if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+         if($this->getUser()) {
+            return $this->redirectToRoute('homepage');
+        }
+
+
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
