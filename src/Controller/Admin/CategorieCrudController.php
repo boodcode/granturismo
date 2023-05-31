02af54ae0ce0554bba7d8a86ef6bc73327e40b84
Matthieu <matt@boodcode.com>
2023-05-31 16:29:50 +0200

    categorie admin crud

--- src/Controller/Admin/CategorieCrudController.php
+++ src/Controller/Admin/CategorieCrudController.php
@@ -3,7 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Categorie;
+use App\Entity\Marque;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
 
 class CategorieCrudController extends AbstractCrudController
 {
@@ -12,14 +17,17 @@ class CategorieCrudController extends AbstractCrudController
         return Categorie::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield IdField::new('id', 'ID')
+            ->onlyOnIndex()
+        ;
+        yield TextField::new('titre');
+        yield AssociationField::new('operations')
+            ->onlyOnIndex()
+        ;
+
     }
-    */
+
 }
